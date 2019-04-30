0b77645da780a64dec88294f1477bde2b2324a16
huangwang1003 <huangwang1003@gmail.com>
2019-04-30 21:30:48 +0800

    Update

--- pages/index/userinfo.js
+++ pages/index/userinfo.js
@@ -4,15 +4,31 @@ $(function(){
         _xsrf: getCookie("_xsrf"),
     },function(datas){
         $("#userinfo").html(datas.user.uname);
-        var str = ''
+
+        var generalstr = ''
         for(o in datas.action){
             aaa = datas.action[o];
-            str += aaa.lx + "=" + aaa.cs + "<br>"
+            generalstr += aaa.lx + "=" + aaa.cs + "<br>"
         }
-        // $("#actioninfo").html(str);
+        $("#general").html(generalstr);
+
+        var entitystr = ''
+        var entitybystr = ''
+        for(p in datas.action){
+            aaa = datas.action[p];
+            if(aaa.lx.startsWith('stat.killEntity')){
+                entitystr += aaa.lx + "=" + aaa.cs + "<br>"
+            }else if(aaa.lx.startsWith('stat.entityKilledBy')){
+                entitybystr += aaa.lx + "=" + aaa.cs + "<br>"
+            }
+        }
+        $("#entity").html(entitystr+"<br>"+entitybystr);
+
     },"json")
 });
 
 
-j = {"stat.useItem.minecraft.quartz_block": "石英块"
+j = {"stat.useItem.minecraft.quartz_block": "石英块",
+    "stat.killEntity.Bat": "你杀死了x只蝙蝠",
+    "stat.entityKilledBy.Bat": "蝙蝠杀死了你x次"
 }
